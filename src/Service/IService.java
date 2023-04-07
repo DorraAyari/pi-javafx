97daa07e6fc3cb5c405cc915b6cb3beaad52b779
Dorra <dorra.ayari@esprit.tn>
2023-04-07 21:45:39 +0100

    new commit

---
+++ src/Service/IService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import java.util.List;
+
+/**
+ *
+ * @author dorraayari
+ */
+//T retourne  un seul objet
+public interface IService<T> {
+    void insert(T t);
+    void delete(int id);
+    void update(T t);
+    List<T>readAll();
+    T readById(int id);
+}
--- src/pi/project/fx/PiProjectFx.java
+++ src/pi/project/fx/PiProjectFx.java
@@ -19,11 +19,34 @@ import javafx.stage.Stage;
  */
 public class PiProjectFx extends Application {
     
+     
     @Override
     public void start(Stage primaryStage) {
-       
-       
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
         
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
     
 }
